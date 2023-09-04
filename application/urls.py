0a2c63053626e0925fe169747ce594e53c197261
ZIXIANG JI <jerryji040506@ucla.edu>
2023-09-04 17:12:30 +0800

    stdudent view application

--- application/urls.py
+++ application/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import ApplicationView
+from .views import Student_ApplicationView
 
 urlpatterns = [
-    path('api/application/<int:application_id>', ApplicationView.as_view(), name='Application'),
-    path('api/application', ApplicationView.as_view(), name='Application'),
-
+    path('api/application/<int:application_id>', Student_ApplicationView.as_view(), name='Application'),
+    path('api/application', Student_ApplicationView.as_view(), name='Application'),
 ]
--- application/views.py
+++ application/views.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -7,22 +7,28 @@ from .models import Application
 from user.models import Student
 from job_post.models import Job_post
 from django.db import transaction
+from datetime import datetime, timedelta
+import pytz
 
-class ApplicationView(APIView):
+class Student_ApplicationView(APIView):
     def post(self, request):
         '''
         @request: 
         {
-        'resume': 'PDF.file',
-        'student_id' : Integer, 
-        'job_id': 'Integer', 
-        'linkedIn': 'url', 
-        'answer' : "Char[]", 
+        'resume': 'PDF.file' (required),
+        'student_id' : Integer (required), 
+        'job_id': 'Integer' (required), 
+        'linkedIn': 'url' (required), 
+        'answer' : "Char[]" (required),
+
         }
         '''
         try:
             # Get the uploaded PDF file data
             pdf_data = request.FILES.get('resume')
+
+            if pdf_data is None:
+                raise ValueError('you must upload your Resume')
             if pdf_data.name.split('.')[-1] != 'pdf':
                 raise ValueError("you must submit an PDF version of your Resume")
             elif pdf_data.size > 5242880: # 5 megabytes
@@ -33,7 +39,7 @@ class ApplicationView(APIView):
                 id = request.data.get('student_id')
                 student_applicant = Student.objects.get(id=id)
             except Student.DoesNotExist:
-                raise ValueError(f'Student who applied this job with id #{id} is not found. The student_id passed in is not correct.')
+                raise ValueError(f'Student who applied this job with id #{id} is not found. The student_id passed in is not correct. So this application fails.')
             
 
             # find job_post 
@@ -41,7 +47,7 @@ class ApplicationView(APIView):
                 id = request.data.get('job_id')
                 job_post = Job_post.objects.get(id=id)
             except Job_post.DoesNotExist:
-                raise ValueError(f'Job post with this id #{id} is not found. The job_id passed in is not correct.')
+                raise ValueError(f'Job post with this id #{id} is not found. The job_id passed in is not correct. So this application fails.')
             
             # Create an Application instance using the request data
             with transaction.atomic():
@@ -69,6 +75,8 @@ class ApplicationView(APIView):
         except Exception as e:
             return JsonResponse({'success': True, "error": f"An error occurred: {str(e)}"}, status=500)
 
+    # either admin view all application
+    # or specific student view its application
     def get(self, request, application_id=None):
         # GET all applications
         if application_id is None:
@@ -88,9 +96,21 @@ class ApplicationView(APIView):
                 return JsonResponse({'success': False, 'error': str(e)}, status=500)
 
         # GET One application with ID provided
+        # Content-type == applcation/json
+            '''
+            @request:
+            student_id: Integer
+            ''' 
         else:
             try:
                 application = Application.objects.get(id=application_id)
+
+                # Authorization for student: 
+                try: 
+                    if request.data.get('student_id') != application.student.id:
+                        raise ValueError('You are not authorized to view this application, because it is not your application.')
+                except: 
+                    raise ValueError('You need to pass in ''student_id: interger'' in the request body for validation')
                 response = {
                     'success': True,
                     'message': f'Successful get this application with ID # {application.id}',
@@ -103,3 +123,56 @@ class ApplicationView(APIView):
             except Exception as e:
                 # Handle unexpected exceptions
                 return JsonResponse({'success': False, 'error': str(e)}, status=500)
+
+
+    #Only student can change its application
+    def put(self, request, application_id):
+        '''
+        @request: 
+        {
+        'student_id' : Integer (required), 
+        'resume': 'PDF.file' (optional),
+        'linkedIn': 'url' (optional), 
+        'answer' : "Char[]" (optional), 
+        }
+        '''
+        try:
+            application = Application.objects.get(id=application_id)
+
+            # authorization & validation: 
+            student_id = int(request.data.get('student_id'))
+            if application.student.id != student_id:
+                raise ValueError('Student is not authorized to change this application because it is not your application.')
+            elif application.status != 'In Progress':
+                raise ValueError('Student is not authorized to change this application because the decision is finalized')       
+            elif application.application_date + timedelta(days=1) < datetime.now(pytz.timezone('UTC')):
+                raise ValueError('Student is not authorized to change this application because the deadline for changing this application has expired.')
+            
+            # Iterate through the request data and update application attributes
+            for key, value in request.data.items():
+                if key == 'resume':
+                    updated_pdf = request.FILES.get('resume')
+                    if updated_pdf and updated_pdf.name.split('.')[-1] != 'pdf':
+                        raise ValueError("You must submit a PDF version of your updated Resume")
+                    elif updated_pdf and updated_pdf.size > 5242880:  # 5 megabytes
+                        raise ValueError("Your updated Resume size exceeds 5MB. Please upload a smaller one")
+                    
+                    # Save the PDF data in the same path if provided
+                    if updated_pdf:
+                        with open(application.resume_path, 'wb') as resume_file:
+                            resume_file.write(updated_pdf.read())
+                elif hasattr(application, key):
+                    setattr(application, key, value)
+            
+            # Save the changes
+            application.save()
+            
+            # Return the updated application data
+            response = {
+                'success': True,
+                'message': f'Successful update for application # {application.id}',
+                'application': application.get_application_detail()
+            }
+            return JsonResponse(response, status=200)
+        except Exception as e:
+            return JsonResponse({'success': False, 'error': str(e)})
