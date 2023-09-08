6a410ceccf257366c8ef5d911fcf0dd443b2ebc7
ZIXIANG JI <jerryji040506@ucla.edu>
2023-09-08 15:45:48 +0800

    change to backend

--- application/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ApplicationView
-
-urlpatterns = [
-    path('api/application/<int:application_id>', ApplicationView.as_view(), name='Student view an Application'),
-    path('api/application', ApplicationView.as_view(), name='Student submit an Application'),
-
-]
--- user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import AlumniView, StudentView
-
-urlpatterns = [
-    path('api/alumni', AlumniView.as_view(), name='Only Superuser can view all Alumni''s profiles'),
-    path('api/alumni/<int:alumni_id>', AlumniView.as_view(), name='Everyone can view a specific Alumni Profile; alumni can only modify&delete his/her own profile'),
-    path('api/student', StudentView.as_view(), name='Only Superuser can view all Student''s profiles'),
-    path('api/student/<int:student_id>', StudentView.as_view(), name='Everyone can view a specific Student Profile; student can only modify&delete his/her own profile'),
-]
